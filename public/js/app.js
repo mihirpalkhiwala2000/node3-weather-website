a1f441a7a1e35d1198b09c67f7307f1dfe78e474
Mihirpalkhiwala2000 <palkhiwalamihir@gmail.com>
2023-06-16 12:50:50 +0530

    2nd commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,30 @@
 console.log('Clent side js file loaded')
 
-fetch('http://localhost:3000/weather?address=boston').then((response)=>{
+
+const weatherForm = document.querySelector('form')
+const search=document.querySelector('input')
+const messageOne=document.querySelector('#message-1')
+const messageTwo=document.querySelector('#message-2')
+
+messageOne.textContent='Loading Message'
+messageTwo.textContent=''
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location=search.value
+    fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
-            console.log(data.error)
+            messageOne.textContent=data.error
+            messageTwo.textContent=""
         }
         else{
 
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent=data.location
+            messageTwo.textContent=data.forecast
         }
         
     })
+})
+
 })
